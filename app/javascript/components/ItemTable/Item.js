e25a8ec6a09d1cd1241b72b309688d9ddaeae401
Jcg408 <jcg321@comcast.net>
2020-02-03 16:02:00 -0500

    add Item component, import Thumbnail and css file

--- app/javascript/components/ItemTable/Item.js
+++ app/javascript/components/ItemTable/Item.js
@@ -1,44 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Thumbnail from '../Image/Thumbnail';
+import '../../../css/item.css';
 
-const Item = (props) => {
+const Item = props => {
     return (
         <div className='container'>
+           <div className='row'>
+            <div className='col-md-10 offset-1' id='thumbnail'>
+                  <div className='card px-5'>
+                           
+                            <div className='row'>
+                              <div className='col-md-4'>
+                                 <Thumbnail />
+                              </div>
+                              <div className='col-md-8'>
+                            <h4>{props.title}</h4>
+                            <p>{props.description}</p>
+                            <div className='cta-wrapper'>
+                              <a className='btn cta-btn'>Watch Video</a>
+                            </div>
+                              </div>
+                              
+            
+                            </div>
 
-
-        <div className="card-deck">
-        <div className="card">
-          <img className="card-img-top" src="..." alt="Card image cap"/>
-          <div className="card-body">
-            <h5 className="card-title">{props.title}</h5>
-            <p className="card-text">{props.description}</p>
-            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-          </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
+    );
+};
 
-<div className="card-deck">
-<div className="card">
-  <img className="card-img-top" src="..." alt="Card image cap"/>
-  <div className="card-body">
-    <h5 className="card-title">{props.title}</h5>
-    <p className="card-text">{props.description}</p>
-    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-</div>
-</div>
-
-<div className="card-deck">
-<div className="card">
-  <img className="card-img-top" src="..." alt="Card image cap"/>
-  <div className="card-body">
-    <h5 className="card-title">{props.title}</h5>
-    <p className="card-text">{props.description}</p>
-    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-</div>
-</div>
-</div>
-      );
-}
- 
 export default Item;
