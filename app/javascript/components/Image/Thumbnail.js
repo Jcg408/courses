5e8b6eaff3092405162de14d47a889b3a6cbfc4f
Jcg408 <jcg321@comcast.net>
2020-02-03 16:00:44 -0500

    add Thumbnail component and import css file

---
+++ app/javascript/components/Image/Thumbnail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../../css/thumbnail';
+
+const Thumbnail = () => {
+    return (
+        <div className='img-thumbnail'>
+           <iframe width="200" height="100" src="https://www.youtube.com/embed/CjLb_t4Tqsw" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
+      );
+}
+ 
+export default Thumbnail;
